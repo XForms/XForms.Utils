68f509202e192c3d5eb580646f06dc7ba088b646
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2016-02-24 00:53:15 +0300

    Added ObservableObject

---
+++ src/XForms.Utils.Tests/Mocks/FakeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XForms.Utils.Mvvm;
+
+namespace XForms.Utils.Tests.Mocks
+{
+	public class FakeViewModel : ObservableObject
+	{
+		private string _value;
+
+		public string Value
+		{
+			get { return _value; }
+			set { SetPropertyChanged(ref _value, value); }
+		}
+	}
+}
---
+++ src/XForms.Utils.Tests/Mvvm/ObservableObjectTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using XForms.Utils.Tests.Mocks;
+
+namespace XForms.Utils.Tests.Mvvm
+{
+	[TestFixture]
+	public class ObservableObjectTests
+	{
+		[TestCase("sasdfg")]
+		public void SetPropertyChanged(string value)
+		{
+			string actual = null;
+			var vm = new FakeViewModel();
+			vm.PropertyChanged += (s, e) =>
+			{
+				actual = e.PropertyName;
+			};
+			
+			vm.Value = value;
+			
+			Assert.IsNotNull(actual);
+			Assert.AreEqual("Value", actual);
+			Assert.AreEqual(value, vm.Value);
+		}
+
+		[Test]
+		public void RaisePropertyChanged()
+		{
+			var propName = "Value";
+
+			string actual = null;
+			var vm = new FakeViewModel();
+			vm.PropertyChanged += (s, e) =>
+			{
+				actual = e.PropertyName;
+			};
+
+			vm.NotifyPropertyChanged(propName);
+
+			Assert.IsNotNull(actual);
+			Assert.AreEqual(propName, actual);
+		}
+	}
+}
