8fb813719b863cb694205ceadf5de091686f00d6
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2016-02-28 21:10:08 +0300

    Added DialogService

---
+++ src/XForms.Utils/Notifications/DialogService.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XForms.Utils.Notifications
+{
+	public class DialogService : IDialogService
+	{
+		public virtual async Task AlertAsync(string title, string message, string cancelButton = "Cancel")
+		{
+			await Application.Current.MainPage.DisplayAlert(title, message, cancelButton);
+		}
+
+		public virtual async Task<bool> ConfirmAsync(string title, string message, string acceptButton = "Yes", string cancelButton = "No")
+		{
+			return await Application.Current.MainPage.DisplayAlert(title, message, acceptButton, cancelButton);
+		}
+	}
+}
---
+++ src/XForms.Utils/Notifications/IDialogService.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+
+namespace XForms.Utils.Notifications
+{
+	public interface IDialogService
+	{
+		/// <summary>
+		/// Presents an alert dialog to the application user with a single cancel button.
+		/// </summary>
+		/// <para>
+		/// The <paramref name="message"/> can be empty.
+		/// </para>
+		/// <param name="title">Title to display.</param>
+		/// <param name="message">Message to display.</param>
+		/// <param name="cancelButton">Text for the cancel button.</param>
+		/// <returns></returns>
+		Task AlertAsync(string title, string message, string cancelButton = "Cancel");
+
+		/// <summary>
+		/// Presents an confirm dialog to the application user with an accept and a cancel button.
+		/// </summary>
+		/// <para>
+		/// The <paramref name="message"/> can be empty.
+		/// </para>
+		/// <param name="title">Title to display.</param>
+		/// <param name="message">Message to display.</param>
+		/// <param name="acceptButton">Text for the accept button.</param>
+		/// <param name="cancelButton">Text for the cancel button.</param>
+		/// <returns><c>true</c> if non-destructive button pressed; otherwise <c>false</c>/></returns>
+		Task<bool> ConfirmAsync(string title, string message, string acceptButton = "Yes", string cancelButton = "No");
+	}
+}
